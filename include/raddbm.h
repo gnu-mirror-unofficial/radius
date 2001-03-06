8b8c16edfaf2d7375cd899aa041b90c4825cb102
Sergey Poznyakoff <gray@gnu.org.ua>
2001-03-06 16:20:18 +0000

    Contains declarations for DBM wrappers from radlib/dbm.c

---
+++ include/raddbm.h
@@ -0,0 +1,32 @@
+/* This file is part of GNU RADIUS.
+ * Copyright (C) 2001, Sergey Poznyakoff
+ *
+ * This file is free software; as a special exception the author gives
+ * unlimited permission to copy and/or distribute it, with or without
+ * modifications, as long as this notice is preserved.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifdef USE_DBM
+
+/* **************************************************************************
+ * DBM wrappers
+ */
+#ifdef NDBM
+# include <ndbm.h>
+typedef DBM *DBM_FILE;
+#else
+# include <dbm.h>
+typedef int DBM_FILE;
+#endif
+
+typedef datum DBM_DATUM;
+
+int open_dbm(char *name, DBM_FILE *dbmfile);
+int close_dbm(DBM_FILE dbmfile);
+int fetch_dbm(DBM_FILE dbmfile, DBM_DATUM key, DBM_DATUM *ret);
+
+#endif

ba9bdccdbc3c916a23c856b4a48bc4174fe94b09
Sergey Poznyakoff <gray@gnu.org.ua>
2001-03-07 10:42:11 +0000

    *** empty log message ***

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,15 @@
 #! /bin/sh
+# This file is part of GNU RADIUS.
+# Copyright (C) 2000,2001, Sergey Poznyakoff
+#
+# This file is free software; as a special exception the author gives
+# unlimited permission to copy and/or distribute it, with or without
+# modifications, as long as this notice is preserved.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# $Id$
 
 if expr ${SOURCEDIR:?} : '\..*' 2>/dev/null 1>&2; then
 	SOURCEDIR="`pwd`/$SOURCEDIR"

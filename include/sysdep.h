7a9ab52781fa0dce830d80fcfcb6971ab84f66bc
Sergey Poznyakoff <gray@gnu.org.ua>
2001-03-20 16:00:27 +0000

    fixed inclusion of <time.h> and <sys/time.h>

--- include/sysdep.h
+++ include/sysdep.h
@@ -43,8 +43,11 @@ typedef unsigned long	UINT4;
 extern int strncasecmp(char*, char*, int);
 #endif
 
-#ifdef TM_IN_SYS_TIME
+#ifdef HAVE_SYS_TIME_H
 # include <sys/time.h>
+# ifdef TIME_WITH_SYS_TIME
+#  include <time.h>
+# endif
 #else
 # include <time.h>
 #endif

52e29eb27e88cc646b01ae69687c09407e790152
Sergey Poznyakoff <gray@gnu.org.ua>
2001-04-12 14:30:44 +0000

    Added keyword and argument completion, context-sesitive help

--- elisp/radconf-mode.el
+++ elisp/radconf-mode.el
@@ -1,6 +1,6 @@
 ;;; radconf-mode.el --- major mode for editing GNU radius raddb/config file
 
-;; Authors: 2000 Sergey Poznyakoff
+;; Authors: 2001 Sergey Poznyakoff
 ;; Version:  1.0
 ;; Keywords: radius
 ;; $Id$
@@ -23,6 +23,22 @@
 ;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
+;; Installation.
+;;  You may wish to use precompiled version of the module. To create it
+;;  run:
+;;    emacs -batch -f batch-byte-compile radconf-mode.el
+;;  Install files radconf-mode.el and radconf-mode.elc to any directory in
+;;  Emacs's load-path.
+
+;; Customization:
+;;  To your .emacs or site-start add:
+;;  (autoload 'radconf-mode "Radius-Config")
+;;  (setq auto-mode-alist (append auto-mode-alist
+;;                                '(("raddb/config$" . radconf-mode))))
+
+;; You may also wish to modify the following variables:
+;;   radconf-level-indent  -- Amount of indentation per block nesting level.
+
 (defvar radconf-mode-syntax-table nil
   "Syntax table used in radconf-mode buffers.")
 
@@ -33,7 +49,8 @@
   (modify-syntax-entry ?\n " " radconf-mode-syntax-table)
   (modify-syntax-entry ?/  ". 14"  radconf-mode-syntax-table)
   (modify-syntax-entry ?*  ". 23"  radconf-mode-syntax-table)
-  (modify-syntax-entry ?\n ">" radconf-mode-syntax-table) )
+  (modify-syntax-entry ?\n ">" radconf-mode-syntax-table)
+  (modify-syntax-entry ?- "w" radconf-mode-syntax-table) )
 
 (defvar radconf-mode-abbrev-table
   nil
@@ -45,14 +62,23 @@
 (if radconf-mode-map
     ()
   (setq radconf-mode-map (make-sparse-keymap))
-  (define-key radconf-mode-map "\t" 'radconf-complete-or-self-insert)
+  (define-key radconf-mode-map "\t" 'radconf-complete-or-indent)
   (define-key radconf-mode-map "\e\t" 'radconf-indent-command)
-  (define-key radconf-mode-map "}" 'radconf-electric-brace) )
+  (define-key radconf-mode-map "}" 'radconf-electric-brace)
+  (define-key radconf-mode-map "?" 'radconf-describe-keywords) )
+
+(defvar radconf-level-indent 8
+  "Amount of additional indentation per nesting level")
 
 (defconst radconf-toplevel-keywords
-  '(option logging auth acct proxy cntl snmp))
+  '(option logging auth acct proxy cntl snmp)
+  "List of the keywords that open their blocks")
 
-(defun radconf-nesting-level ()
+;; Find the block opened by one of the keywords from KEYWORD-LIST,
+;; such that it contains the point.
+;; Return cons whose car is a keyword (or nil if no block was found)
+;; and cdr is the nesting level counted from that block downwards.
+(defun radconf-find-block (keyword-list)
   (save-excursion
     (let ((off (if (progn
 		     (beginning-of-line)
@@ -60,6 +86,7 @@
 		   1
 		 0))
 	  (level 0)
+	  (keyword nil)
 	  (state (list 'start)))
       (while (and (not (eq (car state) 'stop))
 		  (= (forward-line -1) 0))
@@ -89,27 +116,34 @@
 	    (cond
 	     ((string-match "}" string)
 	      (setq state (append (list 'in-block) state)))
-	     ((string-match "/\\*" string)
-	      (if (not (string-match "\\*/" string))
+	     ((string-match "\\*/" string)
+	      (if (not (string-match "/\\*" string))
 		  (setq state (append (list 'in-comment) state))))
 	     ((string-match "{" string)
 	      (beginning-of-line)
 	      (setq level (1+ level))
 	      (cond
 	       ((search-forward-regexp "\\s *\\w+\\s +\\w+\\s *{" bound t)
+		;; Skip `channel' and `category' statements
 		)
 	       ((search-forward-regexp "\\s *\\(\\w+\\)\\s *{" bound t)
 		(let ((word (intern (buffer-substring (match-beginning 1)
 						      (match-end 1)))))
-		  (if (and (memq word radconf-toplevel-keywords)
+		  (if (and (memq word keyword-list)
 			   (null (cdr state)))
-		      (setq state (list 'stop))))))))))))
+		      (setq keyword word
+			    state (list 'stop))))))))))))
       (if (> level 0)
-	  (- level off)
-	level))))
-		    
-(defvar radconf-level-indent 8)
+	  (- level off))
+      (cons keyword level))))
 
+;; Determine the nesting level of point.
+(defun radconf-nesting-level ()
+  (let ((block (radconf-find-block radconf-toplevel-keywords)))
+    (cdr block)))
+		    
+;; Indent current line. Optional LEVEL-OFFSET is subtracted from
+;; the determined amount of indentation.
 (defun radconf-indent-line (&optional level-offset)
   (let* ((start-of-line (save-excursion
 			  (beginning-of-line)
@@ -128,9 +162,11 @@
       (goto-char (+ (point) off))))
 
 (defun radconf-indent-command (arg)
+  "Indent current line"
   (interactive "p")
   (radconf-indent-line))
 
+;; A list of keywords allowed in each block
 (defconst radconf-keyword-dict
   ;; Block     Keyword-list
   '((nil        usedbm
@@ -183,20 +219,33 @@
 		acl)
     (acl        allow
 		deny) ))
-	     
 
+;; Valid successors for keywords.
+(defconst radconf-keyword-successor
+  ;; Keyword                    List of successors
+  '((category			debug info notice warning error
+				crit auth proxy *)
+    (detail			yes no)
+    (strip-names		yes no)
+    (checkrad-assume-logged	yes no)
+    (usedbm			yes no) ))
+
+(defconst radconf-keyword-nodes
+  ;; Block kwd	Info file	Info node
+  '((nil        "radius"	"config")
+    (option     "radius"	"option")
+    (logging    "radius"	"logging")
+    (auth       "radius"	"auth")
+    (acct       "radius"	"acct")
+    (proxy      "radius"	"proxy")
+    (notify     "radius"	"notify")
+    (snmp       "radius"	"snmp") ))
+
+;; Find the topmost block containing the point
 (defun radconf-block ()
-  (save-excursion
-    (if (search-backward "{" nil t)
-	(let ((x (search-backward-regexp "[;{]" nil t)))
-	  (if (null x)
-	      (beginning-of-buffer))
-	  (if (search-forward-regexp "\\(\\w+\\)" nil t)
-	      (let ((word (intern (buffer-substring (match-beginning 1)
-						    (match-end 1)))))
-		(if (assoc word radconf-keyword-dict)
-		    word)))))))
+  (car (radconf-find-block radconf-toplevel-keywords)))
 
+;; Complete a given keyword
 (defun radconf-complete-keyword (word &optional prompt require-match)
   (let ((table (assoc (radconf-block) radconf-keyword-dict)))
     (if table
@@ -208,36 +257,74 @@
 				      nil require-match word nil)))
 	  (or compl word)))))
 
+;; Complete the argument to a keyword.
+(defun radconf-complete-argument (pred word &optional prompt require-match)
+  (let ((table (assoc (intern pred) radconf-keyword-successor)))
+    (if table
+	(let ((compl (completing-read (or prompt "what? ")
+				      (mapcar
+				       (lambda (x)
+					 (cons (symbol-name x) nil))
+				       (cdr table))
+				      nil require-match word nil)))
+	  (or compl word)))))
 
-(defun radconf-complete-or-self-insert (arg)
+(defun radconf-complete-or-indent (arg)
+  "Complete the keyword the point stays on or indent the current line"
   (interactive "p")
   (let* ((here (point))
+	 (off 0)
 	 (bound (save-excursion
 		  (beginning-of-line)
 		  (point))))
     (if (or (search-backward-regexp "^\\W\\(\\w+\\)" bound t)
 	    (search-backward-regexp "^\\(\\w+\\)" bound t))
-	(if (= (match-end 1) here)
-	    (let* ((from (match-beginning 1))
-		   (to (match-end 1))
-		   (word (buffer-substring from to))
-		   (compl (radconf-complete-keyword word nil t)))
-	      (cond
-	       ((and compl (not (string-equal compl word)))
-		(delete-region from to)
-		(goto-char from)
-		(insert compl)
-		(goto-char (+ (point) (- here to))))
-	       (t
-		(goto-char here)) ))
-	  (goto-char here))
-	  (self-insert-command (or arg 1)) )))
+	(let* ((from (match-beginning 1))
+		(to (match-end 1))
+		(word (buffer-substring from to)))
+	  (if (= to here)
+	      ;; Process a keyword
+	      (let ((compl (radconf-complete-keyword word "keyword: ")))
+		(cond
+		 ((and compl (not (string-equal compl word)))
+		  (delete-region from to)
+		  (goto-char from)
+		  (insert compl)
+		  (setq off (- (point) here)))))
+	    ;; Process the argument
+	    (goto-char to)
+	    (if (looking-at "\\s *\\(\\w+\\).*$")
+		(let* ((from (match-beginning 1))
+		       (to (match-end 1))
+		       (arg (buffer-substring from to))
+		       (compl (radconf-complete-argument
+			       word
+			       arg
+			       "argument: ")))
+		  (cond
+		   ((and compl (not (string-equal compl arg)))
+		    (delete-region from to)
+		    (goto-char from)
+		    (insert compl)
+		    (setq off (- (point) here)))))))
+	  (goto-char (+ here off)) )
+	(radconf-indent-line) )))
 
 (defun radconf-electric-brace (arg)
+  "Indent current line and insert a symbol"
   (interactive "p")
   (radconf-indent-line 1)
   (self-insert-command (or arg 1)))
-	  
+
+(defun radconf-describe-keywords ()
+  "Depending on the context invoke appropriate info page"
+  (interactive)
+  (let* ((elt (assoc (radconf-block) radconf-keyword-nodes))
+	 (file (car (cdr elt)))
+	 (node (car (cdr (cdr elt)))))
+    (Info-goto-node (concat "(" file ")" node))
+    (if (get-buffer "*info*")
+	(switch-to-buffer "*info*"))))
 
 ;;;###autoload
 (defun radconf-mode ()
@@ -255,8 +342,7 @@ Key bindings:
 	indent-line-function 'radconf-indent-line
 	completion-ignore-case nil)
 
-  (use-local-map radconf-mode-map)
-  )
+  (use-local-map radconf-mode-map))
   
 (provide 'radconf-mode)
 ;;; radius-mode ends

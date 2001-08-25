de47dcf7bea66089ef16db4fcfbbe6bcd512904e
Sergey Poznyakoff <gray@gnu.org.ua>
2001-08-25 04:21:59 +0000

    Removed sub-shell invocation, fixed paths.

--- test/test.sh
+++ test/test.sh
@@ -18,19 +18,19 @@ if expr ${BUILDDIR:?} : '\..*' 2>/dev/null 1>&2; then
 	BUILDDIR="`pwd`/$BUILDDIR"
 fi
 
-(cd ${BUILDDIR}/test;
- [ -d raddb ] || cp -r ${SOURCEDIR}/test/raddb .
+if [ ! -f raddb/config.in ]; then
+    cp -r ${SOURCEDIR}/test/raddb .
+fi
+EXPR=`./findport -c2 -s1644 "-fs/@AUTH_PORT@/%d/;s/@ACCT_PORT@/%d/"`
+sed $EXPR raddb/config.in > raddb/config
+sed $EXPR raddb/radctl.rc.in > raddb/radctl.rc
 
- EXPR=`./findport -c2 -s1644 "-fs/@AUTH_PORT@/%d/;s/@ACCT_PORT@/%d/"`
- sed $EXPR raddb/config.in > raddb/config
- sed $EXPR raddb/radctl.rc.in > raddb/radctl.rc
+[ -d log ] || mkdir log
+[ -d acct ] || mkdir acct
+cat /dev/null > log/radwtmp
+cat /dev/null > log/radutmp
+RADSCM_BOOTPATH=${SOURCEDIR}/radscm \
+  ../radscm/radscm --debug --directory raddb \
+                   -s ${SOURCEDIR}/test/test.scm \
+                    --build-dir $BUILDDIR
 
- [ -d log ] || mkdir log
- [ -d acct ] || mkdir acct
- cat /dev/null > log/radwtmp
- cat /dev/null > log/radutmp
- RADSCM_BOOTPATH=${SOURCEDIR}/radscm \
-  ${BUILDDIR}/radscm/radscm --debug --directory raddb \
-                             -s ${SOURCEDIR}/test/test.scm \
-                             --build-dir $BUILDDIR
-)

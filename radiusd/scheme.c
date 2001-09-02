5f5bff133dc6e2dc7c2a0a96bbd09c271d9565bf
Sergey Poznyakoff <gray@gnu.org.ua>
2001-09-02 21:07:36 +0000

    Changed USE_GUILE to USE_SERVER_GUILE. Removed library-specific functions.

--- radiusd/scheme.c
+++ radiusd/scheme.c
@@ -24,7 +24,7 @@ static char rcsid[] =
 # include <config.h>
 #endif
 
-#ifdef USE_GUILE
+#ifdef USE_SERVER_GUILE
 
 #include <radiusd.h>
 #include <libguile.h>
@@ -101,6 +101,11 @@ scheme_auth(procname, req, user_check, user_reply_ptr)
 
 	/* Evaluate the procedure */
 	procsym = scm_symbol_value0 (procname);
+	if (scm_procedure_p(procsym) != SCM_BOOL_T) {
+		radlog(L_ERR,
+		       _("procname is not a procedure object"));
+		return 1;
+	}
 	if (setjmp(jmp_env))
 		return 1;
 	res = scm_internal_lazy_catch(SCM_BOOL_T,
@@ -126,6 +131,36 @@ scheme_auth(procname, req, user_check, user_reply_ptr)
 	return 1;
 }
 
+int
+scheme_acct(procname, req)
+	char *procname;
+	RADIUS_REQ *req;
+{
+	SCM procsym, res;
+	jmp_buf jmp_env;
+	SCM s_request = radscm_avl_to_list(req->request);
+
+	/* Evaluate the procedure */
+	procsym = scm_symbol_value0 (procname);
+	if (scm_procedure_p(procsym) != SCM_BOOL_T) {
+		radlog(L_ERR,
+		       _("procname is not a procedure object"));
+		return 1;
+	}
+	if (setjmp(jmp_env))
+		return 1;
+	res = scm_internal_lazy_catch(SCM_BOOL_T,
+				      eval_catch_body,
+				      (void*)SCM_LIST2(procsym,
+						       scm_cons (SCM_IM_QUOTE,
+								 s_request)),
+				      eval_catch_handler, &jmp_env);
+	if (SCM_IMP(res) && SCM_BOOLP(res)) 
+		return res == SCM_BOOL_F;
+	return 1;
+}
+
+
 void
 scheme_load(filename)
 	char *filename;
@@ -143,172 +178,5 @@ scheme_load_path(path)
 			    scm_symbol_value0("%load-path"));
 }
 
-/* The functions below belong to libradscm: */
-SCM 
-scm_makenum (val)
-	unsigned long val;
-{
-  if (SCM_FIXABLE ((long) val))
-    return SCM_MAKINUM (val);
-
-#ifdef SCM_BIGDIG
-  return scm_long2big (val);
-#else /* SCM_BIGDIG */
-  return scm_make_real ((double) val);
-#endif /* SCM_BIGDIG */
-}
-
-SCM
-radscm_avl_to_list(pair)
-	VALUE_PAIR *pair;
-{
-	SCM scm_first = SCM_EOL, scm_last, scm_attr, scm_value, new;
-	
-	for (; pair; pair = pair->next) {
-		new = radscm_avp_to_cons(pair);
-		if (scm_first == SCM_EOL) {
-			scm_last = scm_first = new;
-		} else {
-			SCM_SETCDR(scm_last, new);
-			scm_last = new;
-		}
-	}
-	if (scm_first != SCM_EOL)
-		SCM_SETCDR(scm_last, SCM_EOL);
-	return scm_first;
-}
-
-VALUE_PAIR *
-radscm_list_to_avl(list)
-	SCM list;
-{
-	VALUE_PAIR *first, *last, *p;
-
-	first = last = NULL;
-	do {
-		p = radscm_cons_to_avp(SCM_CAR(list));
-		if (p) {
-			p->next = NULL;
-			if (!last)
-				first = p;
-			else
-				last->next = p;
-			last = p;
-		}
-		list = SCM_CDR(list);
-	} while (list != SCM_EOL);
-	return first;
-}
-
-
-SCM
-radscm_avp_to_cons(pair)
-	VALUE_PAIR *pair;
-{
-	SCM scm_value, new;
-
-	switch (pair->type) {
-	case PW_TYPE_STRING:
-	case PW_TYPE_DATE:
-		scm_value = scm_makfrom0str(pair->strvalue);
-		break;
-	case PW_TYPE_INTEGER:
-		scm_value = scm_makenum(pair->lvalue);
-		break;
-	case PW_TYPE_IPADDR:
-		scm_value = scm_ulong2num(pair->lvalue);
-		break;
-	default:
-		abort();
-	}
-
-	SCM_NEWCELL(new);
-	SCM_SETCAR(new, scm_cons(scm_makenum(pair->attribute), scm_value));
-	return new;
-}
-
-/*
- * (define scm (cons NAME VALUE))
- */
-
-VALUE_PAIR *
-radscm_cons_to_avp(scm)
-	SCM scm;
-{
-	SCM car, cdr;
-	DICT_ATTR *dict;
-	DICT_VALUE *val;
-	VALUE_PAIR pair, *p;
-	
-	if (!(SCM_NIMP(scm) && SCM_CONSP(scm)))
-		return NULL;
-
-	car = SCM_CAR(scm);
-	cdr = SCM_CDR(scm);
-
-	if (SCM_IMP(car) && SCM_INUMP(car)) {
-		pair.attribute = SCM_INUM(car);
-		dict = attr_number_to_dict(pair.attribute);
-		if (!dict) 
-			return NULL;
-		pair.name = dict->name;
-	} else if (SCM_NIMP(car) && SCM_STRINGP(car)) {
-		pair.name = SCM_CHARS(car);
-		dict = attr_name_to_dict(pair.name);
-		if (!dict) 
-			return NULL;
-		pair.attribute = dict->value;
-	} else
-		return NULL;
-	
-	pair.type = dict->type;
-	pair.operator = PW_OPERATOR_EQUAL;
-
-	switch (pair.type) {
-	case PW_TYPE_INTEGER:
-		if (SCM_IMP(cdr) && SCM_INUMP(cdr)) {
-			pair.lvalue = SCM_INUM(cdr);
-		} else if (SCM_BIGP(cdr)) {
-			pair.lvalue = (UINT4) scm_big2dbl(cdr);
-		} else if (SCM_NIMP(cdr) && SCM_STRINGP(cdr)) {
-			char *name = SCM_CHARS(cdr);
-			val = value_name_to_value(name);
-			if (val) {
-				pair.lvalue = val->value;
-			} else {
-				pair.lvalue = strtol(name, &name, 0);
-				if (*name)
-					return NULL;
-			}
-		} else
-			return NULL;
-		break;
-		
-	case PW_TYPE_IPADDR:
-		if (SCM_IMP(cdr) && SCM_INUMP(cdr)) {
-			pair.lvalue = SCM_INUM(cdr);
-		} else if (SCM_BIGP(cdr)) {
-			pair.lvalue = (UINT4) scm_big2dbl(cdr);
-		} else if (SCM_NIMP(cdr) && SCM_STRINGP(cdr)) {
-			pair.lvalue = get_ipaddr(SCM_CHARS(cdr));
-		} else
-			return NULL;
-		break;
-	case PW_TYPE_STRING:
-	case PW_TYPE_DATE:
-		if (!(SCM_NIMP(cdr) && SCM_STRINGP(cdr)))
-			return NULL;
-		pair.strvalue = make_string(SCM_CHARS(cdr));
-		pair.strlength = strlen(pair.strvalue);
-		break;
-	default:
-		abort();
-	}
-
-	p = alloc_entry(sizeof(VALUE_PAIR));
-	*p = pair;
-	
-	return p;
-}
 
 #endif

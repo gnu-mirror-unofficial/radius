a1e3dab8216337d46f9d37715f68351df93cb103
Sergey Poznyakoff <gray@gnu.org.ua>
2001-12-18 16:09:39 +0000

    Print a reminder for first-time installers

--- autogen.sh
+++ autogen.sh
@@ -5,3 +5,10 @@ aclocal -I m4 &&
  autoheader &&
  automake -a -c &&
  autoconf
+
+if [ ! -r include/debugmod.h ]; then
+    echo "NOTE: Now you should do the following:"
+    echo "    ./configure --enable-maintainer-mode [other options]"
+    echo "    make debugmod"
+    echo "    make"
+fi

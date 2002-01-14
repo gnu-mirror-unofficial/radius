b8fa3fff163f67c1326409a3037eb9430b88b017
Sergey Poznyakoff <gray@gnu.org.ua>
2002-01-14 07:34:54 +0000

    Collect port usage statistics.

--- raduse/stat.c
+++ raduse/stat.c
@@ -84,15 +84,73 @@ SNMP_WALK_TAB walk_stat_tab[] = {
 	oid_NASAddress,     offsetof(struct nas_usage, ipaddr),
 	oid_NASID,          offsetof(struct nas_usage, ident),
 	oid_NASLinesInUse,  offsetof(struct nas_usage, ports_active),
-	oid_NASLinesIdle,   offsetof(struct nas_usage, ports_idle),
+	oid_NASLinesIdle,   offsetof(struct nas_usage, ports_idle), 
+	NULL
+};
+
+struct port_usage_header {
+	struct port_usage *head, *tail;
+} port_usage_header;
+
+SNMP_WALK_TAB walk_port_tab[] = {
+	oid_StatPortNASIndex,
+	offsetof(struct port_usage, nas_index),
+	
+	oid_StatPortID,
+	offsetof(struct port_usage, port_no),
+
+	oid_StatPortFramedAddress,
+	offsetof(struct port_usage, framed_address),  
+
+	oid_StatPortTotalLogins,
+	offsetof(struct port_usage, count),  
+
+	oid_StatPortStatus,
+	offsetof(struct port_usage, active), 
+
+	oid_StatPortStatusChangeTimestamp,
+	offsetof(struct port_usage, start),
+	
+//	oid_StatPortUpTime,	           offsetof(struct port_usage, ),  
+
+	oid_StatPortLastLoginName,
+	offsetof(struct port_usage, login),  
+
+	oid_StatPortLastLoginTimestamp,
+	offsetof(struct port_usage, lastin),
+
+	oid_StatPortLastLogoutTimestamp,
+	offsetof(struct port_usage, lastout),  
+
+	oid_StatPortIdleTotalTime,
+	offsetof(struct port_usage, idle),  
+
+	oid_StatPortIdleMaxTime,
+	offsetof(struct port_usage, maxidle.duration), 
+
+	oid_StatPortIdleMaxTimestamp,
+	offsetof(struct port_usage, maxidle.start),  
+	
+	oid_StatPortInUseTotalTime,
+        offsetof(struct port_usage, inuse),  
+
+	oid_StatPortInUseMaxTime,
+	offsetof(struct port_usage, maxinuse.duration), 
+
+	oid_StatPortInUseMaxTimestamp,
+	offsetof(struct port_usage, maxinuse.start),  
+
 	NULL
 };
 
 void *
-nas_usage_alloc(hdr)
+nas_usage_insert(hdr, usg)
 	struct nas_usage_header *hdr;
+	struct nas_usage *usg;
 {
 	struct nas_usage *ptr = emalloc(sizeof(*ptr));
+	memcpy(ptr, usg, sizeof(*ptr));
+	ptr->next = NULL;
 	if (!hdr->head)
 		hdr->head = ptr;
 	else
@@ -101,17 +159,75 @@ nas_usage_alloc(hdr)
 	return ptr;
 }
 
+int
+portcmp(a,b)
+	struct port_usage *a,*b;
+{
+	if (a->nas_index != b->nas_index)
+		return a->nas_index - b->nas_index;
+	return a->port_no - b->port_no;
+}
+
+void
+portcopy(dst, src)
+	struct port_usage *dst, *src;
+{
+	struct port_usage *next;
+	if (dst->login)
+		efree(dst->login);
+	next = dst->next;
+	memcpy(dst, src, sizeof(*dst));
+	dst->next = next;
+}
+
+void *
+port_usage_insert(hdr, data)
+	struct port_usage_header *hdr;
+	struct port_usage *data;
+{
+	struct port_usage *ptr, *prev = NULL;
+	int rc;
+	
+	for (ptr = hdr->head; ptr; ptr = ptr->next) {
+		rc = portcmp(data, ptr);
+		if (rc == 0) {
+			break;
+		} else if (rc < 0) {
+			ptr = NULL;
+			break;
+		}
+		prev = ptr;
+	}
+	if (!ptr) {
+		ptr = emalloc(sizeof(*ptr));
+		if (!prev) {
+			ptr->next = hdr->head;
+			hdr->head = ptr;
+		} else {
+			ptr->next = prev->next;
+			prev->next = ptr;
+		}
+	}
+	portcopy(ptr, data);
+	return ptr;
+}
+
 void
 test()
 {
 	char buf[64];
 	struct nas_usage *nasp;
-/*	
+	struct port_usage *port;
+	char ipbuf[DOTTED_QUAD_LEN];
+		
 	run_query(get_stat_tab);
 	run_query(get_auth_tab);
-*/	
-	run_walk(walk_stat_tab, nas_usage_alloc, &nas_usage_header);
-/*
+
+	run_walk(walk_stat_tab, sizeof(struct nas_usage),
+		 nas_usage_insert, &nas_usage_header);
+	run_walk(walk_port_tab, sizeof(struct port_usage),
+		 port_usage_insert, &port_usage_header);
+	
 	printf("ident: %s\n", stat_ident);
 	printf("uptime: %s\n", format_time(&stat_uptime, buf, sizeof buf));
 
@@ -119,15 +235,39 @@ test()
 	printf("total:  %d\n", stat_total_lines);
 	printf("inuse:  %d\n", stat_lines_in_use);
 	printf("idle :  %d\n", stat_lines_idle);
-*/
+
 	printf("NAS status:\n");
 	for (nasp = nas_usage_header.head; nasp; nasp = nasp->next) {
-		char ipbuf[DOTTED_QUAD_LEN];
 		
 		printf("  ident:  %s\n", nasp->ident);
-		printf("  IP:     %s\n", ipaddr2str(ipbuf, nasp->ipaddr));
+		printf("  IP:     %s\n",
+		       ipaddr2str(ipbuf, ntohl(nasp->ipaddr)));
 		printf("  active: %d\n", nasp->ports_active);
 		printf("  idle:   %d\n", nasp->ports_idle);
+	}
+	printf("\nPort stat:\n");
+	for (port = port_usage_header.head; port; port = port->next) {
+		printf("  index:  %d\n", port->nas_index);  
+		printf("  portno: %d\n", port->port_no);
+		printf("  addr:   %s\n",
+		       ipaddr2str(ipbuf, ntohl(port->framed_address)));
+		printf("  total logins: %d\n", port->count);
+		printf("  state: %d\n", port->active);
+		printf("  change: %s", ctime(&port->start));
+		printf("  last login at %s", ctime(&port->lastin));
+		printf("  last logout at %s", ctime(&port->lastout));
+		printf("  login: %s\n", port->login);
+		printf("  idle total: %s\n",
+		       format_time(&port->idle, buf, sizeof buf));
+		printf("  idle max: %s\n",
+		       format_time(&port->maxidle.duration, buf, sizeof buf));
+		printf("       at: %s", ctime(&port->maxidle.start));
+		printf("  inuse total: %s\n",
+		       format_time(&port->inuse, buf, sizeof buf));
+		printf("  inuse max: %s\n",
+		      format_time(&port->maxinuse.duration, buf, sizeof buf));
+		printf("       at: %s", ctime(&port->maxinuse.start));
+		printf("\n");
 	}
 }
 

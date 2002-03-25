144ba2e21cccf849b0c970b176bb25d4caabb6dc
Sergey Poznyakoff <gray@gnu.org.ua>
2002-03-25 18:24:08 +0000

    Added to the repository

---
+++ include/radargp.h
@@ -0,0 +1,14 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <argp.h>
+
+extern struct argp rad_common_argp;
+extern struct argp_child rad_common_argp_child;
+error_t rad_argp_parse(const struct argp *argp,
+		       int *pargc,
+		       char **pargv[],
+		       unsigned flags,
+		       int *arg_index,
+		       void *input);

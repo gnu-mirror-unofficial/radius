dd98116cec418b12118bffe6ec6db152be149b4a
Sergey Poznyakoff <gray@gnu.org.ua>
2002-05-29 12:26:10 +0000

    (rad_print_request): Format the request into given output buffer. NOTES: Format must be revised. (radlog_req): Output a diagnostic message along with the short request data.

--- lib/logger.c
+++ lib/logger.c
@@ -50,6 +50,105 @@ radlog(lvl, msg, va_alist)
         va_end(ap);
 }
 
+/* Try to represent a request as fully as possible.
+   General format is:
+
+     REQ-TYPE ":" NAS-ID ":" REQ-ID [ ";" REQ-DATA ] "[" USER-NAME "]"
+
+   REQ-TYPE := "AUTH" | "ACCT"
+   NAS-ID := Name of the NAS from naslist or its FQDN, or its IP in dotted-quad
+   notation.
+   REQT-ID := <NUMBER> -- The request identifier.
+
+   If REQ-TYPE is "AUTH", then REQ-DATA is empty. Otherwise,
+
+   REQ-DATA := ACCT-STATUS ";" ACCT-SESSION-ID 
+
+   e.g.:
+   
+   AUTH:nas2:345[uname]
+   ACCT:nas2:346;Start;01012456[uname]
+
+   The maximum length of the output buffer is
+
+   4+1+MAX_LONGNAME+1+4+2*AUTH_STRING_LEN+3+1+AUTH_STRING_LEN+1+1 = 1031 bytes
+*/
+
+char *
+rad_print_request(req, outbuf, size)
+	RADIUS_REQ *req;
+	char *outbuf;
+	size_t size;
+{
+	char nasbuf[MAX_LONGNAME];
+	VALUE_PAIR *stat_pair, *name_pair;
+	char *buf = NULL;
+	char sbuf[2*AUTH_STRING_LEN+3];
+
+	sbuf[0] = 0;
+	stat_pair = avl_find(req->request, DA_ACCT_STATUS_TYPE);
+	if (stat_pair) {
+		VALUE_PAIR *sid_pair = avl_find(req->request,
+						DA_ACCT_SESSION_ID);
+		DICT_VALUE *dval = value_lookup(stat_pair->lvalue,
+						"Acct-Status-Type");
+		char nbuf[64], *stat;
+
+		if (dval)
+			stat = dval->name;
+		else {
+			snprintf(nbuf, sizeof nbuf, "%d", sid_pair->lvalue);
+			stat = sbuf;
+		}
+				 
+		snprintf(sbuf, sizeof sbuf,
+			 " %s %s",
+			 stat, sid_pair ? sid_pair->strvalue : "(none)");
+	}
+
+	name_pair = avl_find(req->request, DA_USER_NAME);
+	snprintf(outbuf, size, "(%s %s %d %s%s)",
+		 auth_code_abbr(req->code),
+		 nas_request_to_name(req, nasbuf, sizeof nasbuf),
+		 req->id,
+		 name_pair ? name_pair->strvalue : "[none]",
+		 sbuf);
+	return outbuf;
+}
+
+/*PRINTFLIKE3*/
+void
+radlog_req(lvl, req, msg, va_alist)
+        int lvl;
+	RADIUS_REQ *req;
+        char *msg;
+        va_dcl
+{
+	va_list ap;
+
+	if (req) {
+		char idbuf[MAXIDBUFSIZE];
+		char *buf = NULL;
+
+		va_start(ap);
+		vasprintf(&buf, msg, ap);
+		va_end(ap);
+	
+		radlog(lvl, "%s: %s",
+		       rad_print_request(req, idbuf, sizeof idbuf),
+		       buf);
+		free(buf);
+	} else {
+		int ec = 0;
+
+		if (lvl & L_PERROR)
+			ec = errno;
+		va_start(ap);
+		vlog(lvl, NULL, 0, NULL, ec, msg, ap);
+		va_end(ap);
+	}		
+}
+
 void
 _dolog(level, file, line, func_name, fmt, va_alist)
         int level;

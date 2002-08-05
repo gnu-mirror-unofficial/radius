11f98fcf207d8c6bdfb37e4d85041b98243bef22
Sergey Poznyakoff <gray@gnu.org.ua>
2002-08-05 08:01:31 +0000

    Allow user-defined attribute properties.

--- lib/dict.c
+++ lib/dict.c
@@ -367,6 +367,17 @@ _dict_attribute(errcnt, fc, fv, file, lineno)
 			case 'c':
 				prop |= AP_REQ_CMP;
 				break;
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				prop |= AP_USER_FLAG(*p-'0');
+				break;
                         case 'Z':
                         case 'I':
                                 break;

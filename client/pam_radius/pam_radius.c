e869c9b50850ce3391256fe5790a34fdf971c437
Sergey Poznyakoff <gray@gnu.org.ua>
2003-01-08 15:22:54 +0000

    Initial implementation of session management

--- client/pam_radius/pam_radius.c
+++ client/pam_radius/pam_radius.c
@@ -32,6 +32,7 @@
 
 #include <radius.h> 
 #include <radpaths.h> 
+#include <radutmp.h>
 
 /* indicate the following groups are defined */
 #define PAM_SM_AUTH
@@ -179,20 +180,26 @@ vlog(level, file, line, func_name, en, fmt, ap)
         openlog("pam_radius", LOG_CONS|LOG_PID, LOG_AUTH);
         vsyslog(priority[level & L_PRIMASK], fmt, ap);
         if (en)
-                syslog(priority[L_MASK(level)], "syserr: %s (%d)",
+                syslog(priority[level & L_PRIMASK], "syserr: %s (%d)",
                        strerror(en), en);
         closelog();
 }
 
-
-
 #define CNTL_DEBUG       0x0001
 #define CNTL_AUDIT       0x0002
 #define CNTL_AUTHTOK     0x0004
-#define CNTL_WAITDEBUG   0x0008
 
-#define CNTL_DEBUG_LEV() (cntl_flags>>16)
-#define CNTL_SET_DEBUG_LEV(cntl,n) (cntl |= ((n)<<16))
+#define CNTL_HANG_OFF         8
+#define CNTL_HANG()           ((cntl_flags>>CNTL_HANG_OFF) & 0xff)
+#define CNTL_SET_HANG(cntl,n) (cntl |= (((n) & 0xff)<<CNTL_HANG_OFF))
+
+#define CNTL_DEBUG_OFF   16
+#define CNTL_DEBUG_LEV() ((cntl_flags>>CNTL_DEBUG_OFF) & 0xff)
+#define CNTL_SET_DEBUG_LEV(cntl,n) (cntl |= (((n) & 0xff)<<CNTL_DEBUG_OFF))
+
+/* Hang modes */
+#define HANG_START 1
+#define HANG_STOP  2
 
 static int cntl_flags;
 static char *radius_confdir = RADDB_DIR;
@@ -271,11 +278,17 @@ _pam_parse(pam_handle_t *pamh, int argc, const char **argv)
                 } else if (!strcmp(*argv,"audit"))
                         ctrl |= CNTL_AUDIT;
                 else if (!strcmp(*argv,"waitdebug"))
-                        ctrl |= CNTL_WAITDEBUG;
+                        CNTL_SET_HANG(ctrl,HANG_START|HANG_STOP);
+		else if (!strncmp(*argv,"hang=",5)) 
+			CNTL_SET_HANG(ctrl,(*argv)[6] - '0');
+		else if (!strcmp(*argv,"hang"))
+			CNTL_SET_HANG(ctrl,HANG_START|HANG_STOP);
                 else if (!strcmp(*argv,"use_authtok"))
                         ctrl |= CNTL_AUTHTOK;
                 else if (!strncmp(*argv,"confdir=",8)) 
                         MAKE_STR(pamh, *argv+8, radius_confdir);
+		else if (!strncmp(*argv,"utmpdir=",8))
+			MAKE_STR(pamh, *argv+8, radlog_dir);
                 else if (!strncmp(*argv,"service_type=",13)) 
                         MAKE_STR(pamh, *argv+13, service_type);
                 else {
@@ -309,12 +322,6 @@ _cleanup_request(pam_handle_t *pamh, void *x, int error_status)
 {
         radreq_free((RADIUS_REQ*)x);
 }
-
-#define TOK_SOURCE_IP  1
-#define TOK_SERVER     2
-#define TOK_SECRET     3 
-#define TOK_TIMEOUT    4 
-#define TOK_RETRY      5
 
 int
 _read_client_config(pam_handle_t *pamh)
@@ -368,8 +375,7 @@ _pam_init_radius_client(pam_handle_t *pamh)
         radius_dir = radius_confdir;
         radpath_init();
         if (dict_init()) {
-                _pam_log(LOG_CRIT,
-                         "dict_init failed");
+                _pam_log(LOG_CRIT, "dict_init failed");
                 return 1;
         }
         rc = _read_client_config(pamh);
@@ -446,12 +452,14 @@ _radius_auth(pam_handle_t *pamh, char *name, char *password)
         switch (authreq->code) {
         case RT_AUTHENTICATION_ACK:
                 break;
+		
         case RT_AUTHENTICATION_REJECT:
                 /* FIXME: radius may have returned Reply-Message attribute.
                  * we should return it to the caller
                  */
                 radreq_free(authreq);
                 return PAM_USER_UNKNOWN;
+		
         default:
                 _pam_log(LOG_CRIT,
                          "received unexpected response: %d", authreq->code);
@@ -476,6 +484,128 @@ _radius_auth(pam_handle_t *pamh, char *name, char *password)
         avl_add_pair(&authreq->request, avp_dup(namepair));
 
         return PAM_SUCCESS;
+}
+
+static void
+add_stat_pairs(VALUE_PAIR *plist)
+{
+	/* FIXME: noop */
+}
+
+static int
+_radius_acct(pam_handle_t *pamh, struct radutmp *ut)
+{
+        RADIUS_SERVER_QUEUE *queue;
+        int retval;
+        VALUE_PAIR *pairs, *add_pair = NULL;
+        RADIUS_REQ *req;
+        DICT_VALUE *dv;
+        int type;
+	
+        retval = pam_get_data(pamh,
+                              "radius_server_queue", 
+                              (const void **)&queue);
+        if (retval != PAM_SUCCESS) {
+                _pam_log(LOG_CRIT, 
+                         "can't get [radius_server_queue]: %s",
+                         pam_strerror(pamh, retval));
+                return PAM_NO_MODULE_DATA;
+        }
+        retval = pam_get_data(pamh,
+                              "radius_pairlist", 
+                              (const void **)&add_pair);
+        if (retval != PAM_SUCCESS && retval != PAM_NO_MODULE_DATA) {
+                _pam_log(LOG_CRIT, 
+                         "can't get [radius_attrlist]: %s",
+                         pam_strerror(pamh, retval));
+                return PAM_SESSION_ERR;
+        }
+	ut->nas_address = ntohl(queue->source_ip);
+        /*
+         * Create accounting request
+         */
+        pairs = NULL;
+        avl_add_pair(&pairs, avp_create(DA_USER_NAME,
+					strlen(ut->login), ut->login, 0));
+
+	avl_add_pair(&pairs, avp_create(DA_NAS_IP_ADDRESS,
+					0, NULL,
+					queue->source_ip));
+	avl_add_pair(&pairs, avp_create(DA_NAS_PORT_ID,
+					0, NULL,
+					ut->nas_port));
+	
+	avl_add_pair(&pairs,
+		     avp_create(DA_ACCT_SESSION_ID,
+				strlen(ut->session_id), ut->session_id,
+				0));
+	
+	/* Add any additional attributes */
+	for (; add_pair; add_pair = add_pair->next) {
+		VALUE_PAIR *p = install_pair(__FILE__, __LINE__,
+                                             add_pair->name, 
+                                             OPERATOR_EQUAL,
+				             add_pair->avp_strvalue);
+		switch (p->attribute) {
+                case DA_LOGIN_IP_HOST:
+                case DA_FRAMED_IP_ADDRESS:
+                        ut->framed_address = htonl(p->avp_lvalue);
+			break;
+			
+                case DA_FRAMED_PROTOCOL:
+                        ut->proto = p->avp_lvalue;
+			break;
+
+                case DA_ACCT_DELAY_TIME:
+                        ut->delay = p->avp_lvalue;
+                        break;
+
+                case DA_NAS_PORT_TYPE:
+			ut->porttype = p->avp_lvalue;
+                        break;
+		}
+		avl_add_pair(&pairs, p);
+	}
+
+	switch (ut->type) {
+	case P_LOGIN:
+		type = DV_ACCT_STATUS_TYPE_START;
+		radutmp_putent(radutmp_path, ut, type);
+		break;
+
+	case P_IDLE:
+		type = DV_ACCT_STATUS_TYPE_STOP;
+		radutmp_putent(radutmp_path, ut, type);
+		avl_add_pair(&pairs,
+			     avp_create(DA_ACCT_SESSION_TIME,
+					0, NULL,
+					ut->duration));
+		add_stat_pairs(pairs);
+	}
+					
+	avl_add_pair(&pairs,
+		     avp_create(DA_ACCT_STATUS_TYPE,
+				0, NULL,
+				type));
+
+	req = rad_clt_send(queue, PORT_ACCT, RT_ACCOUNTING_REQUEST, pairs);
+        if (req == NULL) {
+                _pam_log(LOG_ERR,
+                         "no response from radius server");
+                avl_free(pairs);
+                return PAM_SESSION_ERR;
+        }
+
+        if (req->code == RT_ACCOUNTING_RESPONSE)
+		retval = PAM_SUCCESS;
+	else {
+                _pam_log(LOG_CRIT,
+                         "received unexpected response: %d", req->code);
+		retval = PAM_SESSION_ERR;
+	}
+	
+	radreq_free(req);
+        return retval;
 }
 
 static int
@@ -602,6 +732,19 @@ _pam_get_password(pam_handle_t *pamh, char **password, const char *prompt)
         return retval;
 }
 
+static void
+debug_hook(int mode, const char *file, int line)
+{
+#ifdef MAINTAINER_MODE
+        if (CNTL_HANG() & mode) {
+		int status;
+                _pam_log(LOG_CRIT, "WAITING FOR DEBUG AT %s:%d", file, line);
+                status = 0;
+                while (!status)
+                        status = status;
+        }
+#endif  
+}
 
 /*
  * PAM framework looks for these entry-points to pass control to the
@@ -623,19 +766,11 @@ pam_sm_authenticate(pam_handle_t *pamh,
         int retval;
         char *name;
         char *password;
-
         
         _pam_parse(pamh, argc, argv);
-        
-#ifdef MAINTAINER_MODE
-        if (cntl_flags & CNTL_WAITDEBUG) {
-                _pam_log(LOG_CRIT, "WAITING FOR DEBUG AT %s:%d",
-                         __FILE__, __LINE__);
-                retval = 0;
-                while (!retval)
-                        retval=retval;
-        }
-#endif  
+
+        debug_hook(HANG_START, __FILE__, __LINE__);
+
         DEBUG(100,("enter pam_sm_authenticate"));
 
         for (;;) {
@@ -662,8 +797,7 @@ pam_sm_authenticate(pam_handle_t *pamh,
                 /*
                  * get password
                  */
-                retval = _pam_get_password(pamh, &password,
-                                           "password: ");
+                retval = _pam_get_password(pamh, &password, "password: ");
                 
                 if (retval == PAM_SUCCESS) {
                         retval = _radius_auth(pamh, name, password);
@@ -688,6 +822,111 @@ pam_sm_setcred(pam_handle_t *pamh,
         return PAM_SUCCESS;
 }
 
+static int
+decode_port(const char *tty, int *port)
+{
+	/* FIXME */
+	return PAM_SUCCESS;
+}
+
+static int
+pam_session_common(pam_handle_t *pamh, struct radutmp *ut)
+{
+	int retval;
+	char *user_name;
+	char *tty = NULL;
+
+	memset(ut, 0, sizeof(*ut));
+	time(&ut->time);
+	
+	/* initialize client side */
+	if (_pam_init_radius_client(pamh)) {
+		_pam_log(LOG_ERR, "can't initialize client side");
+		return PAM_AUTHINFO_UNAVAIL;
+	}
+
+	retval = pam_get_item(pamh, PAM_USER, (void *) &user_name);
+	if (retval == PAM_SUCCESS) {
+		if (!user_name) {
+			_pam_log(LOG_NOTICE, "got null username");
+			return PAM_SESSION_ERR;
+		}
+		DEBUG(10, ("username [%s] obtained", user_name));
+	} else {
+		_pam_log(LOG_NOTICE, "can't get username");
+		return retval;
+	}
+
+	retval = pam_get_item(pamh, PAM_TTY, (const void **)&tty);
+	if (retval == PAM_SUCCESS) {
+		if (!tty) {
+			_pam_log(LOG_NOTICE, "got null tty");
+			return PAM_SESSION_ERR;
+		}
+		DEBUG(10, ("tty [%s] obtained", *tty));
+	} else {
+		_pam_log(LOG_NOTICE, "can't get tty");
+	}
+
+	strncpy(ut->login, user_name, sizeof(ut->login));
+	ut->login[sizeof(ut->login)-1] = 0;
+	strncpy(ut->orig_login, user_name, sizeof(ut->orig_login));
+	ut->orig_login[sizeof(ut->orig_login)-1] = 0;
+
+	if (memcmp(tty, "/dev/", 5) == 0)
+		tty += 5;
+
+	retval = decode_port(tty, &ut->nas_port); 
+
+	/* Create a session id. Only one tty can hold a session */
+	snprintf(ut->session_id, sizeof(ut->session_id), "%s", tty);
+	return retval;
+}
+
+PAM_EXTERN int
+pam_sm_open_session(pam_handle_t *pamh, int flags,
+		    int argc, const char **argv)
+{
+	int retval;
+	char *user_name;
+	char *tty = NULL;
+        struct radutmp  ut;
+
+        _pam_parse(pamh, argc, argv);
+        DEBUG(100,("enter pam_sm_open_session"));
+        debug_hook(HANG_START, __FILE__, __LINE__);
+
+	retval = pam_session_common(pamh, &ut);
+	if (retval != PAM_SUCCESS)
+		return retval;
+	ut.type = P_LOGIN;
+	retval = _radius_acct(pamh, &ut);
+        DEBUG(100,("exit pam_sm_open_session: %d", retval));
+        return retval;
+}
+
+PAM_EXTERN int
+pam_sm_close_session(pam_handle_t *pamh, int flags,
+		     int argc, const char **argv)
+{
+	int retval;
+	char *user_name;
+	char *tty = NULL;
+        struct radutmp  ut;
+
+        _pam_parse(pamh, argc, argv);
+        DEBUG(100,("enter pam_sm_close_session"));
+        debug_hook(HANG_STOP, __FILE__, __LINE__);
+
+	retval = pam_session_common(pamh, &ut);
+	if (retval != PAM_SUCCESS)
+		return retval;
+	ut.type = P_IDLE;
+	retval = _radius_acct(pamh, &ut);
+        DEBUG(100,("exit pam_sm_close_session: %d", retval));
+        return retval;
+}
+
 #ifdef PAM_STATIC
 
 struct pam_module _pam_radius_modstruct = {
@@ -695,9 +934,9 @@ struct pam_module _pam_radius_modstruct = {
         pam_sm_authenticate,                 
         pam_sm_setcred,
         NULL,
-        NULL,
-        NULL,
-        NULL
+	pam_sm_open_session,
+	pam_sm_close_session,
+	NULL
 };
 
 #endif

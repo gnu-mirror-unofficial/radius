975c267590f449877b98a3face0bd4db908a0fd0
Sergey Poznyakoff <gray@gnu.org.ua>
2003-01-23 17:11:25 +0000

    Define PAM_SM_SESSION

--- client/pam_radius/pam_radius.c
+++ client/pam_radius/pam_radius.c
@@ -1,12 +1,12 @@
-/* This file is part of GNU RADIUS.
-   Copyright (C) 2001 Sergey Poznyakoff
+/* This file is part of GNU Radius.
+   Copyright (C) 2001,2002,2003 Sergey Poznyakoff
  
-   This program is free software; you can redistribute it and/or modify
+   GNU Radius is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
  
-   This program is distributed in the hope that it will be useful,
+   GNU Radius is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
@@ -36,6 +36,7 @@
 
 /* indicate the following groups are defined */
 #define PAM_SM_AUTH
+#define PAM_SM_SESSION
 
 #ifndef LINUX_PAM
 #include <security/pam_appl.h>

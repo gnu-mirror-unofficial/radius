f3513882c05dd3cf097f17dc199c9ac680107fb2
Sergey Poznyakoff <gray@gnu.org.ua>
2003-04-29 14:00:42 +0000

    Removed slist.[ch]

--- include/slist.h
+++
@@ -1,8 +0,0 @@
-struct slist {
-        struct slist *next;
-};
-
-void free_slist(struct slist *s, void (*f)());
-struct slist * find_slist(struct slist *s, int (*f)(), void *v);
-struct slist * append_slist(struct slist *s, struct slist *e);
-struct slist * reverse_slist(struct slist *s);

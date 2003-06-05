3051a5cd707f208608cc78bd92076a734a93beb0
Sergey Poznyakoff <gray@gnu.org.ua>
2003-06-05 07:04:47 +0000

    Add -I$(top_srcdir)/include to intl/Makefile.in

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,9 @@
 
 autoreconf -f -i -s
 
+mv intl/Makefile.in intl/Makefile.in~
+sed 's,^INCLUDES.*,& -I$(top_srcdir)/include,' intl/Makefile.in~ > intl/Makefile.in
+
 if [ ! -r include/debugmod.h ]; then
     echo "NOTE: Now you should do the following:"
     echo "    ./configure --enable-maintainer-mode [other options]"

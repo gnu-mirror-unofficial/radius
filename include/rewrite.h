3ca5f9ce1ef4cfa8b0f170387d113831e0be6faa
Sergey Poznyakoff <gray@gnu.org.ua>
2003-06-13 07:26:13 +0000

    (radscm_datum_to_scm,(radscm_scm_to_ival,radscm_rewrite_execute): New declarations.

--- include/rewrite.h
+++ include/rewrite.h
@@ -14,3 +14,9 @@ typedef union {
 } Datum;
 
 int interpret(char *fcall, RADIUS_REQ *req, Datatype *type, Datum *datum);
+
+#ifdef RADIUS_SERVER_GUILE
+SCM radscm_datum_to_scm(Datatype type, Datum datum);
+int radscm_scm_to_ival(SCM cell, int *val);
+SCM radscm_rewrite_execute(char *func_name, SCM ARGS);
+#endif

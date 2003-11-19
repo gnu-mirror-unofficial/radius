e931cee616a3fa61750526e5e3d993cd45203f09
Sergey Poznyakoff <gray@gnu.org.ua>
2003-11-19 09:58:26 +0000

    Modified. Thanks Gerald for the suggestion.

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,11 @@
 #! /bin/sh
 
-autoreconf -f -i -s
-
-mv intl/Makefile.in intl/Makefile.in~
-sed 's,^INCLUDES.*,& -I$(top_srcdir)/include,' intl/Makefile.in~ > intl/Makefile.in
-
-if [ ! -r include/debugmod.h ]; then
+autoreconf -f -i -s &&\
+ mv intl/Makefile.in intl/Makefile.in~ &&\
+ sed 's,^INCLUDES.*,& -I$(top_srcdir)/include,' intl/Makefile.in~ > intl/Makefile.in && \
+ if [ ! -r include/debugmod.h ]; then
     echo "NOTE: Now you should do the following:"
     echo "    ./configure --enable-maintainer-mode [other options]"
     echo "    make debugmod"
     echo "    make"
-fi
+ fi

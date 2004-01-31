86fec101fc1da37c2d459eed5b4d2d4d399c6f09
Sergey Poznyakoff <gray@gnu.org.ua>
2004-01-31 15:15:16 +0000

    Debugmod is no longer necessary

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,4 @@
 
 autoreconf -f -i -s &&\
  mv intl/Makefile.in intl/Makefile.in~ &&\
- sed 's,^INCLUDES.*,& -I$(top_srcdir)/include,' intl/Makefile.in~ > intl/Makefile.in && \
- if [ ! -r include/debugmod.h ]; then
-    echo "NOTE: Now you should do the following:"
-    echo "    ./configure --enable-maintainer-mode [other options]"
-    echo "    make debugmod"
-    echo "    make"
- fi
+ sed 's,^INCLUDES.*,& -I$(top_srcdir)/include,' intl/Makefile.in~ > intl/Makefile.in 

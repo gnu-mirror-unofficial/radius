8a3fce62e3a957904b41fa061172aa831b5aa9bd
Sergey Poznyakoff <gray@gnu.org.ua>
2004-02-05 16:04:26 +0000

    *** empty log message ***

--- include/obstack1.h
+++
@@ -1,7 +0,0 @@
-#define obstack_chunk_alloc grad_emalloc
-#define obstack_chunk_free grad_free
-#ifdef HAVE_OBSTACK
-# include <obstack.h>
-#else
-# include <../lib/obstack.h>
-#endif
--- include/regex1.h
+++
@@ -1,5 +0,0 @@
-#ifdef WITH_INCLUDED_REGEX
-# include <../lib/rx.h>
-#else
-# include <regex.h>
-#endif

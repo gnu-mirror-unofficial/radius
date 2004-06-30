f529563cce025a4988fb713729aa65b8d61263f2
Sergey Poznyakoff <gray@gnu.org.ua>
2004-06-30 14:43:35 +0000

    (timersub): New macro

--- include/sysdep.h
+++ include/sysdep.h
@@ -53,6 +53,17 @@
                       (tvp)->tv_sec == (uvp)->tv_sec &&\
                       (tvp)->tv_usec cmp (uvp)->tv_usec)
 #endif
+#ifndef timersub
+# define timersub(a, b, result)                             \
+  do {                                                      \
+    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;           \
+    (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;        \
+    if ((result)->tv_usec < 0) {                            \
+      --(result)->tv_sec;                                   \
+      (result)->tv_usec += 1000000;                         \
+    }                                                       \
+  } while (0)
+#endif
 
 #if !HAVE_DECL_STRNCASECMP
 extern int strncasecmp(char*, char*, int);

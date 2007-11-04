88711fb4d2c3ede2ab02501e12947d66de8104d5
Sergey Poznyakoff <gray@gnu.org.ua>
2007-11-04 13:02:31 +0000

    (grad_md5_calc): input is const char *.

--- include/radius/md5.h
+++ include/radius/md5.h
@@ -12,7 +12,7 @@ void grad_MD5Update(struct MD5Context *context, unsigned char const *buf,
 		    unsigned len);
 void grad_MD5Final(unsigned char digest[16], struct MD5Context *context);
 void grad_MD5Transform(uint32_t buf[4], uint32_t const in[16]);
-void grad_md5_calc(unsigned char *output, unsigned char *input,
+void grad_md5_calc(unsigned char *output, unsigned char const *input,
 		   unsigned int inlen);
 
 typedef struct MD5Context MD5_CTX;
